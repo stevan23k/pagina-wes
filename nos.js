b58ddc132e475da42359cf653241a5db4756f515
stevan23k <111659091+stevan23k@users.noreply.github.com>
2022-11-14 00:46:44 -0500

    2
    Co-Authored-By: JhoanSt0911 <116908158+JhoanSt0911@users.noreply.github.com>

--- nos.js
+++ nos.js
@@ -1,6 +1,13 @@
-
+/*
 function comentar(){
     var element = document.getElementById("body")
     element.classList.toggle(button);
         console.log("has comentado")
+}
+*/
+
+function boton23(){
+    document.getElementById(boton23);{
+        console.log("yuca")
+    }
 }
